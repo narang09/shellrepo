4bd7473d89cbbb62c246ce22a891d55a322fb748
narang09 <khatri.narang09@gmail.com>
2021-08-16 17:10:26 +0530

    laaast coomit

--- jenk-inst.sh
+++ jenk-inst.sh
@@ -3,8 +3,8 @@
 #!/bin/bash
 
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-
-sudo vi /etc/apt
+sudo su
+cd /etc/apt/
 
 sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> sources.list
 
